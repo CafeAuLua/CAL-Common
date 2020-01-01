df48158ac639821fae8e7f6955d65d77d4becc5b
James Upjohn <jammehcow@jammehcow.co.nz>
2020-01-02 09:14:55 +1300

    Add a few basic token types

--- src/main/java/org/cafeaulua/common/vm53/TokenType.java
+++ src/main/java/org/cafeaulua/common/vm53/TokenType.java
@@ -2,7 +2,10 @@ package org.cafeaulua.common.vm53;
 
 public enum TokenType {
     // TODO: list all token names. They don't have to be inline with any spec.
-    INVALID ("invalid", 0);
+    INVALID ("invalid", 0),
+    KEYWORD ("keyword", 1),
+    OERATOR ("operator", 2),
+    STRLIT  ("string_literal", 3);
 
     public final String name;
     public final int id;
